830f3c0efe5d4e3ba1dfa9be8c1eb9a3b5ad8bdd
Corentin Recanzone <corentindevpro@hotmail.com>
2024-11-25 14:01:31 +0100

    m

--- src/telegram.go
+++ src/telegram.go
@@ -2,21 +2,18 @@ package main
 
 import (
 	"log"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 const (
-	TelegramBotToken = "7423967574:AAHUuvNAsvLsTQ6bHMxHuOWxws_LXVeQUHw" // Token du bot Telegram (@AnnonceImmobilierScraperBot)
-	TelegramChannel  = "@annonceimmobiliers"                            // Nom d'utilisateur public du canal public (https://t.me/annonceimmobiliers), pour les tests utilisé : https://t.me/annonceimmobiliersafidim
-
+	TelegramBotToken = "7423967574:AAHUuvNAsvLsTQ6bHMxHuOWxws_LXVeQUHw" // Token du bot Telegram
+	TelegramChannel  = "@annonceimmobiliers"                            // Canal Telegram
+	MaxRetries       = 5                                                // Nombre maximal de tentatives
 )
 
-/**
- * sendTelegramMessageToPublicChannel envoie un message à un canal Telegram public.
- * @param {string} message - Le message à envoyer.
- * @return {void}
- */
+// sendTelegramMessageToPublicChannel envoie un message à un canal Telegram public.
 func sendTelegramMessageToPublicChannel(message string) {
 	// Initialiser le bot Telegram
 	bot, err := tgbotapi.NewBotAPI(TelegramBotToken)
@@ -27,11 +24,29 @@ func sendTelegramMessageToPublicChannel(message string) {
 	// Créer un nouveau message pour le canal
 	msg := tgbotapi.NewMessageToChannel(TelegramChannel, message)
 
-	// Envoyer le message
-	_, err = bot.Send(msg)
-	if err != nil {
-		log.Printf("Erreur lors de l'envoi du message Telegram : %v", err)
-	} else {
-		log.Println("Message envoyé au canal Telegram.")
+	retries := 0
+
+	for {
+		// Envoyer le message
+		_, err = bot.Send(msg)
+		if err != nil {
+			// Vérifier si l'erreur est liée aux limites de débit
+			if apiErr, ok := err.(*tgbotapi.Error); ok && apiErr.RetryAfter > 0 {
+				log.Printf("Trop de requêtes. Réessayer après %d secondes. Scraper mis en pause en attendant", apiErr.RetryAfter)
+				time.Sleep(time.Duration(apiErr.RetryAfter) * time.Second)
+			} else {
+				log.Printf("Erreur lors de l'envoi du message Telegram : %v", err)
+				return
+			}
+		} else {
+			log.Println("Message envoyé au canal Telegram.")
+			return
+		}
+
+		retries++
+		if retries >= MaxRetries {
+			log.Println("Nombre maximal de tentatives atteint. Abandon de l'envoi.")
+			return
+		}
 	}
 }
