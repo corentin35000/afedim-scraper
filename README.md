d8eff47e99f148491e55c4269a566525a0219495
Corentin Recanzone <corentindevpro@hotmail.com>
2024-11-25 16:58:55 +0100

    m

--- README.md
+++ README.md
@@ -14,6 +14,7 @@
 - Guenno
 - LaMotte
 - Kermarrec
+- Nestenn
 
 <br /><br /><br /><br />
 
--- src/processor.go
+++ src/processor.go
@@ -18,6 +18,10 @@ func RunScraper(intervalMinutes int) {
 	processedReferencesFoncia := make(map[string]bool)
 	processedReferencesAgenceDuColombier := make(map[string]bool)
 	processedReferencesLaFrancaiseImmobiliere := make(map[string]bool)
+	processedReferencesGuenno := make(map[string]bool)
+	processedReferencesLaMotte := make(map[string]bool)
+	processedReferencesKermarrec := make(map[string]bool)
+	processedReferencesNestenn := make(map[string]bool)
 
 	for {
 		// Lancer le scraping pour l'agence Afedim
@@ -34,6 +38,18 @@ func RunScraper(intervalMinutes int) {
 
 		// Lancer le scraping pour l'agence La Française Immobilière
 		processAgencyScraping(processedReferencesLaFrancaiseImmobiliere, "https://www.la-francaise-immobiliere.fr/location/?post_types=location&categorie%5B%5D=27&zone%5B%5D=6212&zone%5B%5D=6204&zone%5B%5D=6214&nb_chambres_min=0&nb_chambres_max=&prix_min=0&prix_max=700&submitted=1&o=date-desc&action=load_search_results&wia_6_type=&searchOnMap=0&wia_1_reference=", "LA FRANCAISE IMMOBILIERE", "La Française Immobilière")
+
+		// Lancez le scraping pour l'agence Guenno
+		processAgencyScraping(processedReferencesGuenno, "https://www.guenno.com/biens/recherche?mandate_type=2&realty_type%5B%5D=1&number_room%5B%5D=2&min_surface=&town=RENNES+35000&price_max=700", "GUENNO", "Guenno")
+
+		// Lancer le scraping pour l'agence La Motte
+		processAgencyScraping(processedReferencesLaMotte, "https://www.kermarrec-habitation.fr/location/?post_type=location&false-select=on&99795fbc=&ville%5B%5D=cesson-sevigne-35510&ville%5B%5D=chantepie-35135&ville%5B%5D=rennes-35000&typebien%5B%5D=appartement&budget_max=700&reference=&rayon=0&avec_carte=false&tri=pertinence", "LA MOTTE", "La Motte")
+
+		// Lancer le scraping pour l'agence Kermarrec
+		processAgencyScraping(processedReferencesKermarrec, "https://www.kermarrec-habitation.fr/location/?post_type=location&false-select=on&99795fbc=&ville%5B%5D=cesson-sevigne-35510&ville%5B%5D=chantepie-35135&ville%5B%5D=rennes-35000&typebien%5B%5D=appartement&budget_max=700&reference=&rayon=0&avec_carte=false&tri=pertinence", "KERMARREC", "Kermarrec")
+
+		// Lancer le scraping pour l'agence Nestenn
+		processAgencyScraping(processedReferencesNestenn, "https://immobilier-rennes-centre.nestenn.com/?action=listing&prestige=0&meuble=0&transaction=louer&list_ville=35+Rennes%2C35135+Chantepie%2C35510+Cesson-S%C3%A9vign%C3%A9&list_type=Appartement&type=Appartement&prix_max=700&pieces=2", "NESTENN", "Nestenn")
 
 		// Attendre avant le prochain cycle
 		time.Sleep(time.Duration(intervalMinutes) * time.Minute)
